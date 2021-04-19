e4f8a057787ac863f77da29a0d63854ff007df35
Ilya Fokin <ilyafokinn@yandex.ru>
2021-04-19 04:34:50 +0300

    Билдит проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,3 @@
-// // webpack.config.js
-// const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
-// module.exports = {
-//   entry: {
-//     main: path.resolve(__dirname, './src/js/index.js'),
-//   },
-//   output: {
-//     path: path.resolve(__dirname, './dist'),
-//     filename: '[name].bundle.js',
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       title: 'webpack Boilerplate',
-//       template: path.resolve(__dirname, './src/html/template.html'), // шаблон
-//       filename: 'index.html', // название выходного файла
-//     }),
-//     new CleanWebpackPlugin(),
-//   ],
-//   module: {
-//     rules: [
-//       //JS
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: ['babel-loader'],
-//       }
-//     ]
-//   }
-// }
-
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
